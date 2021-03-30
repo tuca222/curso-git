3e304de6db0df829a4dd332405eef7fc175512f6
Arthur Lorenzetti <tucaa222@gmail.com>
2021-03-30 14:55:16 -0300

    Acrescentada uma mensagem de bom dia!

--- ProjetoTeste/Program.cs
+++ ProjetoTeste/Program.cs
@@ -11,6 +11,7 @@ namespace ProjetoTeste
         static void Main(string[] args)
         {
             Console.WriteLine("Olá mundo!");
+            Console.WriteLine("Bom dia!");
 
             Console.ReadLine();
         }
