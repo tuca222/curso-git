95659f485c954071cbdfd034c1120001734ff7a9
Arthur Lorenzetti <tucaa222@gmail.com>
2021-03-30 16:19:21 -0300

    Excluindo linha bom dia, adicionando: Tenha um bom dia!

--- ProjetoTeste/Program.cs
+++ ProjetoTeste/Program.cs
@@ -11,9 +11,8 @@ namespace ProjetoTeste
         static void Main(string[] args)
         {
             Console.WriteLine("Olá mundo!");
-            Console.WriteLine("Bom dia!");
             Console.WriteLine("Como você está hoje?");
-
+            Console.WriteLine("Tenha um bom dia!");
 
             Console.ReadLine();
         }
