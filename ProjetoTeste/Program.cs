3bdce43e2b1cabc05255e7998f005df7920d2b02
Arthur Lorenzetti <tucaa222@gmail.com>
2021-03-30 16:53:04 -0300

    Acrescentando tracos

--- ProjetoTeste/Program.cs
+++ ProjetoTeste/Program.cs
@@ -10,10 +10,12 @@ namespace ProjetoTeste
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("-----------------------------------");
             Console.WriteLine("Olá mundo!");
             Console.WriteLine("Como você está hoje?");
             Console.WriteLine("Tenha um bom dia!");
             Console.WriteLine("Tchau");
+            Console.WriteLine("-----------------------------------");
 
             Console.ReadLine();
         }
