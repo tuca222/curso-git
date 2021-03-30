ca9af79d54c650bd768a67ea22cc496a7f9b9e70
Arthur <arthur.lorenzetti.rosa@everis.com>
2021-03-30 16:34:13 -0300

    Acrescentada a mensagem Tchau!

--- ProjetoTeste/Program.cs
+++ ProjetoTeste/Program.cs
@@ -13,8 +13,10 @@ namespace ProjetoTeste
             Console.WriteLine("Olá mundo!");
             Console.WriteLine("Como você está hoje?");
             Console.WriteLine("Tenha um bom dia!");
+            Console.WriteLine("Tchau");
 
             Console.ReadLine();
         }
     }
 }
+    
